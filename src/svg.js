0a5f7303a8a1cdc6c6d509ff1d7b044607c71318
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2022-12-22 22:27:41 +0100

    wip

--- src/svg.js
+++ src/svg.js
@@ -2,28 +2,88 @@ import { getAverageColor } from "./color";
 
 const svgNS = "http://www.w3.org/2000/svg";
 
-/**
- * @param {string} path
- * @returns {Promise<HTMLCanvasElement>}
- */
-function loadCanvasWithImage(path) {
+function loadImage(path) {
   return new Promise((resolve) => {
     const img = new Image();
     img.setAttribute("src", path);
 
     img.onload = () => {
-      const imgWidth = img.naturalWidth;
-      const imgHeight = img.naturalHeight;
-
-      const canvas = document.createElement("canvas");
-      canvas.width = imgWidth;
-      canvas.height = imgHeight;
-
-      canvas.getContext("2d")?.drawImage(img, 0, 0);
-
-      resolve(canvas);
+      resolve(img);
     };
   });
+}
+
+/**
+ * @param {number} width
+ * @param {number} height
+ */
+function getDimensions(width, height) {
+  const max = 1000;
+
+  return [width / 2, height / 2];
+
+  if (width < max || height < max) return [width, height];
+
+  // if (width > height) {
+  //   const w
+
+  //   return []
+  // } else {
+  //   return []
+  // }
+}
+
+/**
+ * @param {string} path
+ * @returns {Promise<HTMLCanvasElement>}
+ */
+async function loadCanvasWithImage(path) {
+  const img = await loadImage(path);
+
+  const imgWidth = img.naturalWidth;
+  const imgHeight = img.naturalHeight;
+
+  const canvas = document.createElement("canvas");
+  // canvas.width = imgWidth;
+  // canvas.height = imgHeight;
+
+  const [w, h] = getDimensions(imgWidth, imgHeight);
+
+  // const w = imgWidth / 2;
+  // const h = imgHeight / 2;
+
+  const context = canvas.getContext("2d");
+  if (context === null) throw Error();
+
+  canvas.width = w;
+  canvas.height = h;
+  // Draw the image
+  let containerRatio = h / w;
+  let width = img.naturalWidth;
+  let height = img.naturalHeight;
+  let imgRatio = height / width;
+
+  if (imgRatio > containerRatio) {
+    // image's height too big
+    height = width * containerRatio;
+  } else {
+    // image's width too big
+    width = height / containerRatio;
+  }
+
+  let s = {
+    width: width,
+    height: height,
+    offsetX: (img.naturalWidth - width) * 0.5,
+    offsetY: (img.naturalHeight - height) * 0.5,
+  };
+  context.drawImage(img, s.offsetX, s.offsetY, s.width, s.height, 0, 0, w, h);
+
+  // context.drawImage(img, 0, 0);
+
+  return canvas;
+  // };
+  // });
 }
 
 /**
